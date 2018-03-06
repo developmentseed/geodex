9ceff5e125a4b7840b5dbad6ccfc56fcba22f213
Mark Wronkiewicz <wronk@developmentseed.org>
2018-03-06 11:36:01 +0000

    Add test for tms and quadtree; incorporate tile format

--- test/test_core.py
+++ test/test_core.py
@@ -3,14 +3,12 @@ test_utils.py
 
 @author: DevelopmentSeed
 
-Test all utilities.
+Test all utilities for GeoDex.
 """
 import os
 import unittest
 from unittest.mock import patch
 from io import StringIO
-
-from pygeotile.tile import Tile
 
 from geodex.search import tile_search, process_geojson
 from geodex.utils import load_geojson
@@ -28,6 +26,14 @@ class TestCore(unittest.TestCase):
                      '2494 2125 12\n'
                      '2493 2126 12\n'
                      '2493 2125 12\n')
+        tile_inds_tms_zxy = ('12-2494-1969\n'
+                             '12-2494-1970\n'
+                             '12-2493-1969\n'
+                             '12-2493-1970\n')
+        tile_inds_quad_tree = ('300112113330\n'
+                               '300112113312\n'
+                               '300112113321\n'
+                               '300112113303\n')
 
         # Load geojson, get 0th object
         shape_json = load_geojson('./roi_single.geojson')[0]
@@ -35,6 +41,16 @@ class TestCore(unittest.TestCase):
         num_tiles = tile_search(shape_json, max_zoom=12)
         self.assertEqual(num_tiles, 4)
         self.assertEqual(mock_stdout.getvalue(), tile_inds)
+        mock_stdout.seek(0)  # XXX: hacky way to reset StringIO object
+        mock_stdout.truncate(0)
+
+        num_tiles = tile_search(shape_json, 12, 'tms', '{z}-{x}-{y}')
+        self.assertEqual(mock_stdout.getvalue(), tile_inds_tms_zxy)
+        mock_stdout.seek(0) # XXX: hacky way to reset StringIO object
+        mock_stdout.truncate(0)
+
+        num_tiles = tile_search(shape_json, 12, 'quad_tree')
+        self.assertEqual(mock_stdout.getvalue(), tile_inds_quad_tree)
 
 
     @patch('sys.stdout', new_callable=StringIO)
@@ -53,6 +69,7 @@ class TestCore(unittest.TestCase):
 
         shape_json = load_geojson('./roi_triple.geojson')
         num_tiles = process_geojson(shape_json, max_zoom=12,
-                                    tile_format='google')
+                                    tile_format='google',
+                                    output_format='{x} {y} {z}')
         self.assertEqual(num_tiles, 10)
         self.assertEqual(mock_stdout.getvalue(), tile_inds)
